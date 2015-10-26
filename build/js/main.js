7f9b05c909bc407053878e56c2d8dfa9210878f6
laurenfazah <laurenfazah@gmail.com>
2015-10-25 22:00:52 -0400

    carousel in

--- build/js/main.js
+++ build/js/main.js
@@ -1,8 +1,22 @@
 $(function(){
-    console.log('js');
+
+    /*//////////////////////////////////////
+    //  burger open close
+    //////////////////////////////////////*/
     $('div').on('click', function(){
-        console.log("zombie");
         $('nav ul').toggleClass('active');
+    });
+
+    /*//////////////////////////////////////
+    //  fun facts carousel
+    //////////////////////////////////////*/
+    $('.slick-it').slick({
+        slide: '.sliding',
+        dots: true,
+        arrows: true,
+        infinite: true,
+        // slidesToShow: 3,
+        // slidesToScroll: 3
     });
 
 });
--- html/js/main.js
+++ html/js/main.js
@@ -1,5 +1,10 @@
 $(function() {
-    console.log("js"), $("div").on("click", function() {
-        console.log("zombie"), $("nav ul").toggleClass("active");
+    $("div").on("click", function() {
+        $("nav ul").toggleClass("active");
+    }), $(".slick-it").slick({
+        slide: ".sliding",
+        dots: !0,
+        arrows: !0,
+        infinite: !0
     });
 });
