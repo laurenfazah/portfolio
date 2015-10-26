bc5f752e82cb9389f721a19a0f6cff6e6f8d281f
laurenfazah <laurenfazah@gmail.com>
2015-10-25 22:34:07 -0400

    fixed node start script for heroku deploy

---
+++ index.js
@@ -0,0 +1,23 @@
+/**
+ * Static HTTP Server
+ *
+ * Create a static file server instance to serve files
+ * and folder in the './public' folder
+ */
+
+// modules
+var static = require( 'node-static' ),
+    http = require( 'http' );
+
+// config
+var file = new static.Server( './html', {
+    cache: 3600,
+    gzip: true
+} );
+
+// serve
+http.createServer( function ( request, response ) {
+    request.addListener( 'end', function () {
+        file.serve( request, response );
+    } ).resume();
+} ).listen( process.env.PORT || 5000 );
