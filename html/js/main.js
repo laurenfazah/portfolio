bebdbb2e5f8cc567f9e035355b42ba34b0ec5421
laurenfazah <laurenfazah@gmail.com>
2015-10-28 12:26:04 -0400

    toggle on who am i and cleaned up fun facts carousel

--- html/js/main.js
+++ html/js/main.js
@@ -1,6 +1,8 @@
 $(function() {
     $(window).width() < 800 && $("nav").on("click", function() {
         $("nav ul").toggleClass("active");
+    }), $(".who").on("click", function() {
+        $(".who p").toggleClass("active");
     }), $(".slick-it").slick({
         slide: ".slick-it li",
         dots: !1,
