8e3abf0775ade96e39d23f86087733f37800b390
laurenfazah <laurenfazah@gmail.com>
2015-10-28 16:46:27 -0400

    structure of resume in place, minimal styling

--- html/js/main.js
+++ html/js/main.js
@@ -1,32 +1,12 @@
-$(function(){
-
-    /*//////////////////////////////////////
-    //  burger open close
-    //////////////////////////////////////*/
-    if($(window).width() < 800){
-        $('nav').on('click', function(){
-            $('nav ul').toggleClass('active');
-        });
-    }
-
-    /*//////////////////////////////////////
-    //  intro section
-    //////////////////////////////////////*/
-    $('.who').on('click', function(){
-        // open who am i
-        $('.who p').toggleClass('active');
-        // flip arrow
-        $('.who img').toggleClass('face-up');
+$(function() {
+    $(window).width() < 800 && $("nav").on("click", function() {
+        $("nav ul").toggleClass("active");
+    }), $(".who").on("click", function() {
+        $(".who p").toggleClass("active"), $(".who img").toggleClass("face-up");
+    }), $(".slick-it").slick({
+        slide: ".slick-it li",
+        dots: !1,
+        arrows: !0,
+        infinite: !0
     });
-
-    /*//////////////////////////////////////
-    //  fun facts carousel
-    //////////////////////////////////////*/
-    $('.slick-it').slick({
-        slide: '.slick-it li',
-        dots: false,
-        arrows: true,
-        infinite: true,
-    });
-
 });
