46ff5c062fda86d2156db749789579a5d4594064
laurenfazah <laurenfazah@gmail.com>
2015-11-04 14:17:30 -0500

    jquery flip working on resume cards

--- html/js/main.js
+++ html/js/main.js
@@ -3,7 +3,7 @@ $(function() {
         $("nav ul").toggleClass("active");
     }), $(".who").on("click", function() {
         $(".who p").toggleClass("active"), $(".who img").toggleClass("face-up");
-    }), $(".slick-it").slick({
+    }), $(".card").flip({}), $(".slick-it").slick({
         slide: ".slick-it li",
         dots: !1,
         arrows: !0,
