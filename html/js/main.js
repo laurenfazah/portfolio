ddeb09536546fb146dd3de4b0e48db3451b01455
laurenfazah <laurenfazah@gmail.com>
2015-11-04 16:47:59 -0500

    added resume content in

--- html/js/main.js
+++ html/js/main.js
@@ -1,61 +1,22 @@
-$(function(){
-
-    /*//////////////////////////////////////
-    //  burger open close
-    //////////////////////////////////////*/
-    if($(window).width() < 800){
-        $('nav').on('click', function(){
-            $('nav ul').toggleClass('active');
-        });
-    }
-
-    var closeMenu = function(){
-        if ($('nav ul').hasClass('active')){
-            $('nav ul').removeClass('active');
-        }
+$(function() {
+    $(window).width() < 800 && $("nav").on("click", function() {
+        $("nav ul").toggleClass("active");
+    });
+    var closeMenu = function() {
+        $("nav ul").hasClass("active") && $("nav ul").removeClass("active");
     };
-
-    $('header').on('click', function(){
+    $("header").on("click", function() {
         closeMenu();
-    });
-
-    $('main').on('click', function(){
+    }), $("main").on("click", function() {
         closeMenu();
-    });
-
-    $('footer').on('click', function(){
+    }), $("footer").on("click", function() {
         closeMenu();
+    }), $(".who").on("click", function() {
+        $(".who p").toggleClass("active"), $(".who img").toggleClass("face-up");
+    }), $(".card").flip({}), $(".slick-it").slick({
+        slide: ".slick-it li",
+        dots: !1,
+        arrows: !0,
+        infinite: !0
     });
-
-    /*//////////////////////////////////////
-    //  intro section
-    //////////////////////////////////////*/
-    $('.who').on('click', function(){
-        // open who am i
-        $('.who p').toggleClass('active');
-        // flip arrow
-        $('.who img').toggleClass('face-up');
-    });
-
-    /*//////////////////////////////////////
-    //  resume section
-    //////////////////////////////////////*/
-    // $('#card').on('click', function(){
-    //     console.log("click");
-    //     $('this').flip();
-    // });
-    $(".card").flip({
-      // axis: 'x',
-      // trigger: 'click'
-    });
-    /*//////////////////////////////////////
-    //  fun facts carousel
-    //////////////////////////////////////*/
-    $('.slick-it').slick({
-        slide: '.slick-it li',
-        dots: false,
-        arrows: true,
-        infinite: true,
-    });
-
 });
