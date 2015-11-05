a1896eb338a9027f23cca8478c36a5e0a3992d0e
laurenfazah <laurenfazah@gmail.com>
2015-11-05 14:51:40 -0500

    changed primary colors

--- html/js/main.js
+++ html/js/main.js
@@ -1,10 +1,22 @@
-$(function() {
-    $("div").on("click", function() {
-        $("nav ul").toggleClass("active");
-    }), $(".slick-it").slick({
-        slide: ".sliding",
-        dots: !0,
-        arrows: !0,
-        infinite: !0
+$(function(){
+
+    /*//////////////////////////////////////
+    //  burger open close
+    //////////////////////////////////////*/
+    $('div').on('click', function(){
+        $('nav ul').toggleClass('active');
     });
+
+    /*//////////////////////////////////////
+    //  fun facts carousel
+    //////////////////////////////////////*/
+    $('.slick-it').slick({
+        slide: '.sliding',
+        dots: true,
+        arrows: true,
+        infinite: true,
+        // slidesToShow: 3,
+        // slidesToScroll: 3
+    });
+
 });
