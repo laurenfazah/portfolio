655465b880d9da5a38dd8d09d6d72a21a640a5aa
laurenfazah <laurenfazah@gmail.com>
2015-11-15 21:29:28 -0500

    reclaimed lost mailgun script

--- html/index.php
+++ html/index.php
@@ -1,3 +1,25 @@
+<?php
+    require '../vendor/autoload.php';
+    use Mailgun\Mailgun;
+    session_start();
+    if(isset($_POST['submit'])){
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $message =  $_POST['message'];
+        # Instantiate the client.
+        $mgClient = new Mailgun('key-3e522d2966a5fbb738696e453ccde349');
+        $domain = "mail.laurenfazah.com";
+        # Make the call to the client.
+        $result = $mgClient->sendMessage($domain, array(
+            'from'    => $name . ' <' . $email . '>',
+            'to'      => 'Lauren <laurenfazah@gmail.com>',
+            'subject' => 'Portfolio Message',
+            'text'    => $message
+        ));
+    }
+    session_destroy();
+?>
+
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en-US">
     <head>
--- html/js/main.js
+++ html/js/main.js
@@ -1,118 +1,41 @@
-$(function(){
-
+$(function() {
     var $document = $(document);
-
-    /*//////////////////////////////////////
-    //  burger open close
-    //////////////////////////////////////*/
-
-    if($(window).width() < 800){
-        $('nav').on('click', function(){
-            $('nav ul').toggleClass('active');
-        });
-    }
-
-    var closeMenu = function(){
-        if ($('nav ul').hasClass('active')){
-            $('nav ul').removeClass('active');
-        }
-    };
-
-    $('header, main, footer').on('click', function(){
-        closeMenu();
+    $(window).width() < 800 && $("nav").on("click", function() {
+        $("nav ul").toggleClass("active");
     });
-
-    /*//////////////////////////////////////
-    //  page scrolling
-    //////////////////////////////////////*/
-
-    $('.scroll-to > a').on('click', function(){
-        var scrollTo = $(this).attr('href');
-        $('html, body').animate({
+    var closeMenu = function() {
+        $("nav ul").hasClass("active") && $("nav ul").removeClass("active");
+    };
+    $("header, main, footer").on("click", function() {
+        closeMenu();
+    }), $(".scroll-to > a").on("click", function() {
+        var scrollTo = $(this).attr("href");
+        $("html, body").animate({
             scrollTop: parseInt($(scrollTo).offset().top)
         }, 500);
-    });
-
-    $('.scroll-to.top').on('click', function(){
-        $('html, body').animate({
+    }), $(".scroll-to.top").on("click", function() {
+        $("html, body").animate({
             scrollTop: 0
         }, 500);
-    });
-
-    $document.scroll(function() {
-        if ($document.scrollTop() >= 400) {
-            $('.scroll-to.top').css('opacity', '.75');
-        } else {
-            $('.scroll-to.top').css('opacity', '0');
-        }
-
-        if ($document.scrollTop() >= 100) {
-            $('nav').removeClass('splash');
-        } else {
-            $('nav').addClass('splash');
-        }
-    });
-
-
-    /*//////////////////////////////////////
-    //  intro section
-    //////////////////////////////////////*/
-
-    $('.who').on('click', function(){
-        // open who am i
-        $('.who p').toggleClass('active');
-        // flip arrow
-        $('.who img').toggleClass('face-up');
-    });
-
-    /*//////////////////////////////////////
-    //  resume section
-    //////////////////////////////////////*/
-    // jquery flip()
-    $(".card").flip();
-
-
-    /*//////////////////////////////////////
-    //  fun facts carousel
-    //////////////////////////////////////*/
-
-    $('.slick-it').slick({
-        slide: '.slick-it li',
-        dots: false,
-        arrows: true,
-        infinite: true,
-    });
-
-    /*//////////////////////////////////////
-    //  portfolio carousel
-    //////////////////////////////////////*/
-
-    $('.portfolio-carousel').slick({
-        dots: false,
-        infinite: true,
+    }), $document.scroll(function() {
+        $document.scrollTop() >= 400 ? $(".scroll-to.top").css("opacity", ".75") : $(".scroll-to.top").css("opacity", "0"), 
+        $document.scrollTop() >= 100 ? $("nav").removeClass("splash") : $("nav").addClass("splash");
+    }), $(".who").on("click", function() {
+        $(".who p").toggleClass("active"), $(".who img").toggleClass("face-up");
+    }), $(".card").flip(), $(".slick-it").slick({
+        slide: ".slick-it li",
+        dots: !1,
+        arrows: !0,
+        infinite: !0
+    }), $(".portfolio-carousel").slick({
+        dots: !1,
+        infinite: !0,
         speed: 300,
         slidesToShow: 1,
         slidesToScroll: 1,
-        arrow: true
-    });
-
-    /*//////////////////////////////////////
-    //  contact form
-    //////////////////////////////////////*/
-
-    $('form[name="contact_form"]').submit(function(){
-        // $('input[name="name"], input[name="email"], textarea[name="message"]').val('');
-        // $('.email-thanks').text('thanks for the message!');
-    });
-
-    /*//////////////////////////////////////
-    //  link out
-    //////////////////////////////////////*/
-    $('.click-thru').each(function(){
+        arrow: !0
+    }), $('form[name="contact_form"]').submit(function() {}), $(".click-thru").each(function() {
         var linkContent = $(this).html();
-        linkContent += '<img class="link-icon" src="img/link_out.svg"\/>';
-        $(this).html(linkContent);
+        linkContent += '<img class="link-icon" src="img/link_out.svg"/>', $(this).html(linkContent);
     });
-
-
 });
